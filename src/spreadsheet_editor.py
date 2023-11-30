4841a29abc7cae8b36b0ebec4e365cd8ba6c9e8e
Lakshay <lakshay21060@iiitd.ac.in>
2023-11-30 11:11:09 +0530

    Fix #3: Progress tracking and updation works

--- src/spreadsheet_editor.py
+++ src/spreadsheet_editor.py
@@ -27,34 +27,29 @@ def get_sheet():
     try:
         service = build("sheets", "v4", credentials=credentials)
         return service.spreadsheets()
-        
-        result = sheets.values().get(spreadsheetId=SPREADSHEET_ID, 
-                                     range="A1:Z6666").execute()
-        values = result.get("values", [])
     except HttpError as error:
         print(error)
         exit(1)
     return values, sheets
 
-def itoc(index):
-    return chr(ord(letter)+index)
+itoc = lambda i: chr(65+i)
 
 def process_sheet(sheets):
     values = sheets.values().get(spreadsheetId=SPREADSHEET_ID, 
             range="A1:Z6666").execute().get("values", [])
-    status = values[0][2: ]
-    users = values[1][2: ]
-    skip = int(values[0][1])
+    status = values[0][1: ]
+    users = values[1][1: ]
+    skip = int(values[0][0])
     week_number = skip % QUESTIONS_PER_WEEK 
     print(f"week number = {week_number}")
     alive_users = dict()
     for i in range(len(users)):
         if status[i] == "Alive":
-            alive_users[users[i]] = 2+i
+            alive_users[users[i]] = 1+i
     print(f"alive users = {alive_users}")
     this_weeks_questions = dict()
     for i in range(QUESTIONS_PER_WEEK):
-        this_weeks_questions[values[2+skip+i][1]] = 2+skip+i
+        this_weeks_questions[values[2+skip+i][0]] = 2+skip+i
     print(f"this week's questions = {this_weeks_questions}")
     for user in alive_users.keys():
         for ques in profile_scrapper.get_questions(user):
@@ -62,12 +57,13 @@ def process_sheet(sheets):
                 continue
             user_index = alive_users[user]
             ques_index = this_weeks_questions[ques]
-            if (values[ques_index][user_index] == "Done"):
+            if user_index < len(values[ques_index]) and \
+                values[ques_index][user_index] == "Done":
                 continue
-            print(f"changing status to done for {user}, {ques}")
+            print(f"{user} did the question '{ques}'!")
             sheets.values().update(spreadsheetId=SPREADSHEET_ID, 
                 range=f"{itoc(user_index)}{ques_index+1}", 
-                valueInputOption="USER_ENTERED", body={"values": [["Done"]]})
+                valueInputOption="USER_ENTERED", body={"values": [["Done"]]}).execute()
 
 if __name__ == "__main__":
     process_sheet(get_sheet())
