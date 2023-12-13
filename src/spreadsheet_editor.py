acae0604a4d2144ef44d1997725dde5244b95991
Lakshay <108230497+Hyouteki@users.noreply.github.com>
2023-12-13 20:32:03 +0530

    increased QUESTIONS_PER_WEEK to 10

--- src/spreadsheet_editor.py
+++ src/spreadsheet_editor.py
@@ -8,7 +8,7 @@ import profile_scrapper
 
 SCOPES = ["https://www.googleapis.com/auth/spreadsheets"]
 SPREADSHEET_ID = "1NcB1bmKRg-j56KUsd7WNHYpkVBx0S7Jkr26LyeJW8HQ"
-QUESTIONS_PER_WEEK = 4
+QUESTIONS_PER_WEEK = 10
 
 def get_sheet():
     credentials = None
